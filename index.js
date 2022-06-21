5de60dc5896c203755fbc0d69eb4294dc3e1fe19
MaiRizk <mai.riizk@gmail.com>
2022-06-21 15:09:01 +0200

    Create mobile menu, style it and add open functionality

---
+++ index.js
@@ -0,0 +1,18 @@
+let menuIcon = document.getElementById("img");
+
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+
+
+  menuIcon.addEventListener("click", openNav);
+
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+
