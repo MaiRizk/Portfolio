82a24e5a581eea1287803a6dab6b2f03f83883d7
MaiRizk <mai.riizk@gmail.com>
2022-06-22 09:48:08 +0200

    Fix linters errors

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById('myNav').style.width = '100%';
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+  document.getElementById('myNav').style.width = '0%';
 }
 
-let menuIcon = document.getElementById("img");
-let closebtn = document.getElementById("closebtn");
-let menuLinks = document.getElementById("overlay-content");
+let menuIcon = document.getElementById('img');
+let closebtn = document.getElementById('closebtn');
+let menuLinks = document.getElementById('overlay-content');
 
-menuIcon.addEventListener("click", openNav);
-closebtn.addEventListener("click", closeNav);
-menuLinks.addEventListener("click", closeNav);
+menuIcon.addEventListener('click', openNav);
+closebtn.addEventListener('click', closeNav);
+menuLinks.addEventListener('click', closeNav);
